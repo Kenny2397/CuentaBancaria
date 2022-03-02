ecdb70b2e0ca50b6307164008a345bf5161da37d
Kenny Luque <kenny.luque.t@uni.pe>
2022-03-02 15:32:20 -0500

    1

--- User.py
+++ User.py
@@ -1,7 +1,25 @@
 from CuentaBancaria import CuentaBancaria
 
-class User:
-    
-    def __init__(self, nombre):
-        self.nombre=nombre
-        # self.cuenta = 
+class User: 
+
+    def __init__(self, nombre, email):
+        self.nombre = nombre
+        self.email = email
+        self.cuenta = CuentaBancaria(2, 1000)
+
+    def hacer_deposito(self, monto):
+        self.balance += monto
+        return self
+
+    def hacer_retiro(self, monto):
+        self.balance -= monto
+        return self
+
+    def mostrar_balance_usuario(self):
+        print(f"Usuario:{self.nombre}, Balance:{self.balance}")
+        return self
+
+    def transf_dinero(self, otro_usuario, monto):
+        self.balance -= monto
+        otro_usuario.balance += monto
+        return self 
